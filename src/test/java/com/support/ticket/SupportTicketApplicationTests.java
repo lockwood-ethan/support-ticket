786d29b37b69095ed923e55d21de3c2c0920e1dc
Ethan Lockwood <lockwood.ethan@yahoo.com>
2026-01-02 14:46:16 -0500

    initial commit

---
+++ src/test/java/com/support/ticket/SupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.support.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
